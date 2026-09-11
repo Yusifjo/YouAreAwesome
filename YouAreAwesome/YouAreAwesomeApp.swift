5cc8145bba0aa1d80b0e2a1120c136b9ddf4da3b
Yusifjo <yibrahimov86@gmail.com>
2026-09-11 19:03:51 +0200

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Joseph ibrahimov on 11.09.26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
