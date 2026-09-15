e04842f34f361049d06f57f00c321ab96bf4e710
Yusifjo <yibrahimov86@gmail.com>
2026-09-15 18:50:24 +0200

    Added "I'm a Developer"

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+            Text("I'm a Developer!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
